3fd94a0880982e6c891ce7bc1f19e4c6dc35f0ed
liuqixin <2057473561@qq.com>
2025-07-03 22:53:20 +0800

    first commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default 
+{
+  // site-level options
+  title: 'VitePressBlog',
+  description: 'vitepress based blog',
+
+  themeConfig: {
+    // theme-level options
+  }
+}
